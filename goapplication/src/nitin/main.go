b32cc61d033aae51bd97c5231af9eb0fb6e6a470
Nitin Rana <nitin.rana@sap.com>
2018-12-27 16:28:59 +0100

    updated version

--- goapplication/src/nitin/main.go
+++ goapplication/src/nitin/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	host, _ := os.Hostname()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Nitin Rana, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Hello Nitin Rana, you've requested: %s\n", host)
 	})
 
 	http.ListenAndServe(":80", nil)
